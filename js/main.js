5baaa9922cfd6cb057f7910c8b8fc98373301232
H - Komila <komilaxibziddinova06@gmail.com>
2025-12-03 23:28:25 +0500

    html to'g'irladim

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const navigation = [
     {
 
-        links: ["Home", "Shop", "Plant Care", "Blogs"],
+         links: [
+            { name: "Home", href: "index.html" },
+            { name: "Shop", href: "shop.html" },
+            { name: "Plant Care", href: "plant-care.html" },
+            { name: "Blogs", href: "blogs.html" }
+        ],
         button: "Login",
         bars: "fa-solid fa-bars"
     }
@@ -14,7 +19,7 @@ function addNavigation() {
         item.links.forEach(link => {
             result += `
                         <li class="nav__item">
-                            <a href="#">${link}</a>
+                            <a href="${link.href}">${link.name}</a>
                         </li>
                     `;
         });
@@ -881,36 +886,6 @@ renderFooter();
 
         
         document.addEventListener('DOMContentLoaded', initializeProductData);
-
-
-
-
-
-// ===========================description-=========================
-
-
-const addDescription = [
-    {
-        title: ["Product Description", "Reviews (19)"],
-        text: "The ceramic cylinder planters come with a wooden stand to help elevate your plants off the ground. The ceramic cylinder planters come with a wooden stand to help elevate your plants off the ground. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla augue nec est tristique auctor. Donec non est at libero vulputate rutrum. Morbi ornare lectus quis justo gravida semper. Nulla tellus mi, vulputate adipiscing cursus eu, suscipit id nulla.Pellentesque aliquet, sem eget laoreet ultrices, ipsum metus feugiat sem, quis fermentum turpis eros eget velit. Donec ac tempus ante. Fusce ultricies massa massa. Fusce aliquam, purus eget sagittis vulputate, sapien libero hendrerit est, sed commodo augue nisi non neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor, lorem et placerat vestibulum, metus nisi posuere nisl, in accumsan elit odio quis mi. Cras neque metus, consequat et blandit et, luctus a nunc. Etiam gravida vehicula tellus, in imperdiet ligula euismod eget. The ceramic cylinder planters come with a wooden stand to help elevate your plants off the ground. ",
-        title1: "Living Room:",
-        text1: "The ceramic cylinder planters come with a wooden stand to help elevate your plants off the ground. The ceramic cylinder planters come with a wooden stand to help elevate your plants off the ground. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        title1: "Dining Room:",
-        text1:"The benefits of houseplants are endless. In addition to cleaning the air of harmful toxins, they can help to improve your mood, reduce stress and provide you with better sleep. Fill every room of your home with houseplants and their restorative qualities will improve your life.",
-        title1: "Office:",
-        text1:"The ceramic cylinder planters come with a wooden stand to help elevate your plants off the ground. The ceramic cylinder planters come with a wooden stand to help elevate your plants off the ground. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    }
-]
-
-
-
-
-
-
-
-
-
-
 
 
 
